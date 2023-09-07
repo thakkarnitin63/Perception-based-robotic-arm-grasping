03e6c4ff155aebe985e15b6b13881add25c6715b
Nitin Thakkar <thakkarnitin63@gmail.com>
2023-09-07 10:30:23 -0400

    fixed ws error

--- realsense_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nitin/realsense_ws/src
-# Build directory: /home/nitin/realsense_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("realsense2_description")
-subdirs("realsense_gazebo_plugin")
--- realsense_ws/build/realsense2_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nitin/realsense_ws/src/realsense2_description
-# Build directory: /home/nitin/realsense_ws/build/realsense2_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
