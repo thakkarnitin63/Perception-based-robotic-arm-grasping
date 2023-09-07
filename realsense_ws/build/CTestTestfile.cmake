6418dc745ac9b7587e7f886b8f88a75d9353ae11
Nitin Thakkar <thakkarnitin63@gmail.com>
2023-09-07 00:00:52 -0400

    added ws

---
+++ realsense_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/nitin/realsense_ws/src
+# Build directory: /home/nitin/realsense_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense2_description")
+subdirs("realsense_gazebo_plugin")
---
+++ realsense_ws/build/realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nitin/realsense_ws/src/realsense2_description
+# Build directory: /home/nitin/realsense_ws/build/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
